a3d3afab7aabe1679e98734d58f7b919e9cdf8de
LingLiufu@gmail.com <LingLiufu@gmail.com@e583b945-71a7-124f-f22c-4411078389d5>
2011-01-19 13:22:33 +0000

    增加收藏夹界面。

---
+++ trunk/Favorite/FavList.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+
+public partial class Favorite_FavList : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if ((Request["page"] + "") != "")
+        {
+            Response.Write("page:" + Request["page"]);
+            Response.End();
+        }
+    }
+}

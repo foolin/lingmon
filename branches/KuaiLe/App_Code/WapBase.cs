917b6d5320e54a89142ca8d30d61408ea3f44619
LingLiufu@gmail.com <LingLiufu@gmail.com@e583b945-71a7-124f-f22c-4411078389d5>
2011-03-08 13:58:04 +0000

    新增手机版访问。

---
+++ branches/KuaiLe/App_Code/WapBase.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+using KuaiLe.Us.Common;
+using Utility.Web;
+
+/// <summary>
+///WapBase 的摘要说明
+/// </summary>
+public class WapBase : System.Web.UI.Page
+{
+	public WapBase()
+	{
+		//
+		//TODO: 在此处添加构造函数逻辑
+		//
+	}
+
+
+    public void WritePage(string title,  string body)
+    {
+
+        StringBuilder strWapContent = new StringBuilder();
+        strWapContent.Append("<div class=\"header\">");
+        strWapContent.Append("" + WapUtil.WriteImage("images/mlogo.gif", "default.aspx", ""));
+        strWapContent.Append("</div>");
+        strWapContent.Append("<div class=\"nav\">");
+        strWapContent.Append("" + WapUtil.WriteLink("首页","default.aspx") + " | ");
+        strWapContent.Append("" + WapUtil.WriteLink("最热", "default.aspx?type=hot") + " | ");
+        strWapContent.Append("" + WapUtil.WriteLink("最新", "default.aspx?type=new") + " | ");
+        //strWapContent.Append("" + WapUtil.WriteLink("注册", "default.aspx?type=new") + " | ");
+        //strWapContent.Append("" + WapUtil.WriteLink("登录", "default.aspx?type=new") + " | ");
+        strWapContent.Append("" + WapUtil.WriteLink("关于", "about.aspx") + " ");
+        strWapContent.Append("</div>");
+
+        strWapContent.Append("<div class=\"container\">");
+        strWapContent.Append(body);
+        strWapContent.Append("</div>");
+
+        strWapContent.Append("<div class=\"footer\">");
+        strWapContent.Append("  &#169; " + DateTime.Now.Year + " 快乐网(www.kuaile.us) <br /> ");
+        strWapContent.Append(" " + DateTime.Now.ToString("yyyy年M月d日 HH:mm:ss"));
+        strWapContent.Append("</div>");
+
+        if (title == "")
+        {
+            title = "我们一起分享";
+        }
+
+        string strHead = "<link rel=\"stylesheet\" href=\"images/wap.css\" type=\"text/css\"/> \r\n";
+        string strWapPage = WapUtil.GetWap2Page("手机快乐网 - " + title, strHead, strWapContent.ToString());
+        Response.Write(strWapPage);
+    }
+
+
+
+    /// <summary>
+    /// 获取Request.QueryString参数
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    protected string QS(string key)
+    {
+        return Request[key] + "";
+    }
+
+
+    /// <summary>
+    /// 获取Request.QueryString参数(整型)
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    protected int QSInt(string key)
+    {
+        return WebAgent.IsNumeric(QS(key)) ? Convert.ToInt32(QS(key)) : 0;
+    }
+
+}
---
+++ branches/KuaiLe/wap/default.aspx.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+using KuaiLe.Us.Common;
+using System.Data;
+using KuaiLe.Us.BLL;
+using System.Text.RegularExpressions;
+
+public partial class wap_default : WapBase
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        string type = "";   //列表类型
+        int userID = -1;
+
+        type = QS("type").ToLower().Trim();
+        userID = QSInt("userid");
+
+        int page = QSInt("page");
+        if (page == 0)
+        {
+            page = 1;
+        }
+
+
+        string strWhere = " (Status> 0 Or  (Status = 0 And CreateTime<='" + DateTime.Now.AddMinutes(-10) + "') )";
+        string strOrder = "ArtID DESC";
+        if (userID > 0)
+        {
+            strWhere += " And UserID=" + userID + " ";
+        }
+
+        if (type == "new")
+        {
+            strOrder = " CreateTime desc ";
+        }
+        else if (type == "hot")
+        {
+            strOrder = " (IsNull(DigUp,0) + IsNull(DigDown,0)) desc ";
+        }
+        else if (type == "up")
+        {
+            strOrder = " DigUp desc ";
+        }
+        else if (type == "down")
+        {
+            strOrder = " DigDown desc ";
+        }
+        else if (type == "comments")
+        {
+            strOrder = " Comments desc ";
+        }
+
+        int record = 0;
+        int pageSize = 15;
+        DataSet dsList = new ArticleBll().GetList(strWhere, strOrder, pageSize, page - 1, out record);
+
+        StringBuilder sb = new StringBuilder();
+        if (dsList != null && dsList.Tables.Count > 0 && dsList.Tables[0].Rows.Count > 0)
+        {
+            sb.Append("<dl class=\"artlist\">");
+            foreach (DataRow dr in dsList.Tables[0].Rows)
+            {
+                if ((dr["Content"] + "").Length > 500)
+                {
+                    continue;
+                }
+                sb.Append("<dt> # " + dr["ArtID"] + "乐</dt>");
+                sb.Append("<dd>" + dr["Content"] + "</dd>");
+                sb.Append("<dd class=\"info\">" + dr["NickName"] + " 发布于 " + dr["CreateTime"] + "</dd>");
+            }
+            sb.Append("</dl>");
+
+            MakePager(sb, page, pageSize, record);
+        }
+
+        WritePage("首页", sb.ToString());
+    }
+
+
+
+    /// <summary>
+    /// 分页导航
+    /// </summary>
+    /// <param name="sb"></param>
+    /// <param name="strPage"></param>
+    /// <param name="pageIndex"></param>
+    /// <param name="pageSize"></param>
+    /// <param name="totalRecords"></param>
+    protected string MakePager(StringBuilder sb, int pageIndex, int pageSize, int totalRecords)
+    {
+        //总页数
+        int pageTotal = 0;
+
+        if (pageSize > 0 && totalRecords > 0)
+        {
+            pageTotal = (int)(totalRecords / pageSize) + 1;
+        }
+
+        if (pageTotal <= 0)
+        {
+            pageTotal = 1;
+        }
+
+        //页数
+        int pagePreNum = 0, pageCurrNum = 0, pageNextNum = 0;
+
+        pagePreNum = pageIndex - 1;     //上一页
+        pageCurrNum = pageIndex;        //当前页
+        pageNextNum = pageIndex + 1;    //下一页
+
+        if (pagePreNum < 1)
+        {
+            pagePreNum = 1;
+        }
+        if (pageNextNum < 1)
+        {
+            pageNextNum = 1;
+        }
+
+        if (pagePreNum > pageTotal)
+        {
+            pagePreNum = pageTotal;
+        }
+        if (pageNextNum > pageTotal)
+        {
+            pageNextNum = pageTotal;
+        }
+
+
+        sb.Append("<form name=\"form1\" action=\"" + GetPageUrl(0) + "\" method=\"get\">");
+
+        sb.Append(WapUtil.WriteLink("[下一页]", GetPageUrl(pageNextNum)));
+        sb.Append(WapUtil.WriteLink("[上一页]", GetPageUrl(pagePreNum)));
+        sb.Append(WapUtil.WriteBreakLine(1));
+
+        sb.Append("第" + pageIndex + "/" + pageTotal + "页");
+        sb.Append(WapUtil.WriteSpace(1));
+        
+        sb.Append("<input type=\"text\" value=\"\" style=\"width:30px;\" name=\"page\" />");
+        sb.Append("<input type=\"submit\" value=\"GO\" name=\"submit\" />");
+        sb.Append("</form>");
+
+        sb.Append(WapUtil.WriteBreakLine(1));
+
+
+        return sb.ToString();
+    }
+
+
+
+    /// <summary>
+    /// 获取当前网址前缀
+    /// </summary>
+    /// <returns></returns>
+    private string GetPageUrl(int pageNum)
+    {
+        string url = HttpContext.Current.Request.Url.PathAndQuery;
+        url = Regex.Replace(url, ("&{0,1}page=\\d*").ToString(), "", RegexOptions.IgnoreCase);
+
+        if (pageNum >= 0)
+        {
+            if (url.IndexOf('?') != -1)
+            {
+                url += "&page=" + pageNum;
+            }
+            else
+            {
+                url += "?page=" + pageNum;
+            }
+        }
+        return url;
+    }
+
+
+
+}

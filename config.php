a410144058d1ca989a51b3aba0758ecae623f4cb
LingLiufu@gmail.com <LingLiufu@gmail.com@e583b945-71a7-124f-f22c-4411078389d5>
2011-06-27 01:19:51 +0000

    增加发送邮件功能。

--- config.php
+++ config.php
@@ -3,28 +3,43 @@
 !defined('IN_LINGLIB') && exit('Access Denied');
 
 $config = array(
-  'debug' => 1,
+	//是否调试
+	'debug' => 1,
 
-  'site_name' => '曾在网',
-  'site_domain' => 'cengzai.com',
-  'site_url' => 'http://cengzai.test.com',
+	//网站
+	'site_name' => '曾在网',
+	'site_domain' => 'cengzai.com',
+	'site_url' => 'http://cengzai.test.com',
 
-  'msg_time' => 5,
+	//消息框
+	'msg_time' => 5,
 
-   //模板
-  'template_path' => 'default',
-  'template_root_path' => './templates/',
-  'compiled_root_path' => './cache/templates/',
-  
-  
-  //数据库
-  'db_host' => '127.0.0.1:3306',
-  'db_user' => 'root',
-  'db_pwd' => '123456',
-  'db_name' => 'cengzai',
-  'db_table_prefix' => 'cz_',
-  'db_charset' => 'utf8',
+	//模板
+	'template_path' => 'default',
+	'template_root_path' => './templates/',
+	'compiled_root_path' => './cache/templates/',
+
+
+	//数据库
+	'db_host' => '127.0.0.1:3306',
+	'db_user' => 'root',
+	'db_pwd' => '123456',
+	'db_name' => 'cengzai',
+	'db_table_prefix' => 'cz_',
+	'db_charset' => 'utf8',
+
+
+	//Smtp发送邮件
+	'smtp_server'=>'smtp.exmail.qq.com',
+	'smtp_port'=>'25',
+	'smtp_user' => 'noreply@cengzai.com',
+	'smtp_password' => 'lingmon',
+	'smtp_from' => 'noreply@cengzai.com',
+
 
 )
+
+
+
 
 ?>
